e65e9218667b6bf848e2f9eac7660a557c9fe693
TheFlameflo <nicolas.sc@outlook.com>
2019-10-21 21:50:05 -0400

    Add Register page
    fixed css and columns positions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/navigation/Navigation'
 import Homepage from './pages/Homepage'
 import Dashboard from './pages/Dashboard'
 import NoMatch from './pages/NoMatch'
+import Register from './pages/Register'
 
 
 export class App extends Component {
@@ -12,7 +13,8 @@ export class App extends Component {
       <Router>
         <div>
           <Switch>
-            <Route exact path={["/dashboard"]} component={Dashboard} />
+            <Route exact path={["/tableaudebord"]} component={Dashboard} />
+            <Route exact path={["/inscription"]} component={Register} />
             <Route exact path={["/", "/accueil"]} component={Homepage} />
             <Route component={NoMatch} />
           </Switch>
