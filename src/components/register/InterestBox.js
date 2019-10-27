6afb0b1bb98b2451cf30e86927f80f1db19c4ac4
TheFlameflo <nicolas.sc@outlook.com>
2019-10-26 22:28:43 -0400

    interest box

--- src/components/register/InterestBox.js
+++ src/components/register/InterestBox.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 export class InterestBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {activated: false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            activated: !prevState.activated
+        }))        
+    }
     render() {
         return (
-            <div>
-                
+            <div className="column is-one-fifth-desktop is-half-touch">
+                <div className="interest-box">
+                    <div className="interest-box-adjuster " >
+                        <div className={"interest-box-content " + this.state.activated == true ? 'interest-box-activated' : ''} onClick={this.handleClick}>
+                            <i className={"interest-icon fas " + this.props.icon}></i>
+                            <i className="activated-icon fas fa-check"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/pages/register/RegisterVolunteer.js
+++ src/pages/register/RegisterVolunteer.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
 import HomepageNavigation from '../../components/navigation/HomepageNavigation';
+import InterestBox from '../../components/register/InterestBox';
 
 export class RegisterVolunteer extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
     render() {
         return (
             <div className="container">
                 <HomepageNavigation />
                 <section className="section has-text-centered">
+                    <div className="column is-offset-1 is-10">
                     <h1 className="title is-size-1 register-title">Je suis un bénévole</h1>
                     <p>Clique sur les lignes pour y ajouter tes informations.</p>
-                    <form class="register-form">
-                        <p className="is-size-4">Je m'appelle <input type="text" name="firstname" placeholder="prénom" /> <input type="text" name="lastname" placeholder="nom de famille" /></p>
-                        <p className="is-size-4">Je suis né le <input type="date" name="dateofbirth"></input> et mon adresse est <input type="text" name="address" placeholder="adresse" /></p>
-                        <p className="is-size-4">Mon adresse email est le <input type="email" name="email" placeholder="adresse email" /></p>
+                    <form className="register-form">
+                        <p className="is-size-4">Je m'appelle <input type="text" name="firstname" placeholder="prénom" value={this.state.firstname} onChange={this.handleChange}/> <input type="text" name="lastname" placeholder="nom de famille" value={this.state.lastname} onChange={this.handleChange}/></p>
+                        <p className="is-size-4">Je suis né le <input type="date" name="dateofbirth" value={this.state.dateofbirth} onChange={this.handleChange}></input> et mon adresse est <input type="text" name="address" placeholder="adresse" value={this.state.address} onChange={this.handleChange}/></p>
+                        <p className="is-size-4">Mon adresse email est le <input type="email" name="email" placeholder="adresse email" value={this.state.email} onChange={this.handleChange} /></p>
                     </form>
+                    </div>
                     <h2 className="register-title subtitle is-size-3">Mes centres d'intérêts</h2>
                     <p>Sélectionne tous tes intérêts en cliquant desuss.</p>
-                    <div className="columns register-columns is-multiline column is-offset-1 is-10">
-                        <div className="column interest-box is-one-fifth"><div className="interest-box-content"></div></div>
-                        <div className="column interest-box is-one-fifth"></div>
-                        <div className="column interest-box is-one-fifth"></div>
+                    <div className="columns interest-columns is-multiline column is-offset-1 is-10 is-mobile">
+                        <InterestBox icon="fa-futbol" />
                     </div>
                 </section>
             </div>
