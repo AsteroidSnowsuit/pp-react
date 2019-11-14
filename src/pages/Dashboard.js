05f6dbbc3307641a3ca6bfd88ed8bf722abd903e
TheFlameflo <nicolas.sc@outlook.com>
2019-11-13 23:22:38 -0500

    Basic Sashboard Structure

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import DashboardNavigation from '../components/navigation/DashboardNavigation'
+import $ from 'jquery'
 
 export class Dashboard extends Component {
+    componentDidUpdate() {
+        $('body').addClass('dashboard');
+    }
     render() {
         return (
-            <div>
+            <div class="container dashboard">
                 <DashboardNavigation />
-                This is the dashboard for connected users.
+                
             </div>
         )
     }
