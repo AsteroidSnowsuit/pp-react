b6b82d01532cbebf8600d20a3cc25ce7f45b0133
TheFlameflo <nicolas.sc@outlook.com>
2019-12-20 23:45:04 -0500

    Handle Clicking on Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
 export class Button extends Component {
+
     render() {
         var size = (this.props.size != undefined ? ' is-' + this.props.size : '');
         var shape = (this.props.shape != undefined ? ' is-' + this.props.shape : '');
@@ -9,7 +10,7 @@ export class Button extends Component {
         var loading = (this.props.loading == true ? ' is-loading' : '');
         var buttonType = (this.props.noSubmit == true ? 'button' : 'submit');
         return (
-            <button type={buttonType} className={"button" + size + shape + type + loading}>{this.props.children}</button>                
+            <button type={buttonType} onClick={this.props.onClick} className={"button" + size + shape + type + loading}>{this.props.children}</button>                
         )
     }
 }
