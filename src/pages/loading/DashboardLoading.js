a8177c110849a218d4fd96ad4dc23c84cf06f384
TheFlameflo <nicolas.sc@outlook.com>
2020-01-01 21:11:23 -0500

    Loading Issue

--- src/pages/loading/DashboardLoading.js
+++ src/pages/loading/DashboardLoading.js
@@ -4,8 +4,9 @@ import LoadingScreen from 'react-loading-screen';
 
 export class DashboardLoading extends Component {
     render() {
+        var loading = (this.props.loading != undefined ? this.props.loading : false) 
         return (
-            <LoadingScreen loading={this.props.loading} bgColor="#e8e8e8" textColor="black" spinnerColor="#0375B4" text="Chargement de la page...">
+            <LoadingScreen loading={loading} bgColor="#e8e8e8" textColor="black" spinnerColor="#0375B4" text="Chargement de la page...">
                 {this.props.children}
             </LoadingScreen>
         )
--- src/pages/loading/HomepageLoading.js
+++ src/pages/loading/HomepageLoading.js
@@ -4,8 +4,9 @@ import LoadingScreen from 'react-loading-screen';
 
 export class HomepageLoading extends Component {
     render() {
+        var loading = (this.props.loading != undefined ? this.props.loading : false) 
         return (
-            <LoadingScreen loading={this.props.loading} bgColor="#0375B4" textColor="white" spinnerColor="#FFBA00" text="Chargement de la page...">
+            <LoadingScreen loading={loading} bgColor="#0375B4" textColor="white" spinnerColor="#FFBA00" text="Chargement de la page...">
                 {this.props.children}
             </LoadingScreen>
         )
